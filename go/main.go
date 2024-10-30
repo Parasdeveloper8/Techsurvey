d8d29717dbf99030819a5982bd8b8c5f78670172
Paras prajapat <parasprajapat8th.gmail.com>
2024-10-31 00:34:34 +0530

    survey landing page completed

--- go/main.go
+++ go/main.go
@@ -13,11 +13,17 @@ func main() {
 	router.Static("/static", "./static")
 
 	router.LoadHTMLGlob("templates/*")
+
 	router.GET("/", func(c *gin.Context) {
 		// Pass data to the template
 		c.HTML(http.StatusOK, "index.html", gin.H{
 			"Name": "User",
 		})
 	})
+
+	router.GET("/surveypage", serveSurveyPageRoute)
 	router.Run(":4700")
+}
+func serveSurveyPageRoute(c *gin.Context) {
+	c.HTML(http.StatusOK, "surveypages.html", nil)
 }
